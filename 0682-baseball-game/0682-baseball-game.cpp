6a45e33c46ff5853dd75f74bbd2f57087accf3aa
Loveneesh <loveneeshmeena666@gmail.com>
2026-08-09 20:38:52 +0530

    Time: 0 ms (100%), Space: 12.9 MB (18.39%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        
+
+        stack<int>st;
+
+     for(string op:operations){
+        if(op == "C"){
+            st.pop();
+        }
+        else if(op == "D"){
+           int x = st.top();
+           st.push(2*x);
+        }
+        else if(op=="+"){
+            int first = st.top();
+            st.pop();
+
+            int second = st.top();
+
+            st.push(first);
+
+            st.push(first+second);
+        }
+        else{
+            st.push(stoi(op));
+        }
+     }
+     int ans =0;
+     while(!st.empty()){
+        ans+= st.top();
+        st.pop();
+     }
+     return ans;
+    }
+};
